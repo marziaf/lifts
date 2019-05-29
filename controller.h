e5abdf30e8a7ffe21f8518d8758c03b0878a25e0
Marzia Favaro <maarzia.f@gmail.com>
2019-05-29 23:08:22 +0200

    controller.h with statusw

---
+++ controller.h
@@ -0,0 +1,32 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "elevator.h"
+#include "queue.h"
+#include "parameters.h"
+
+typedef struct status {
+	// Each element represents a floor, and is a pointer to a queue
+	person_t *floors_queues[FLOORS];
+	// Two elevators
+	elevator_t *elevator1;
+	elevator_t *elevator2;
+} status_t;
+
+
+/** Inserts a customer in the system
+ * @param status: the system the customer has to be inserted into
+ * @param from: where the customer comes from
+ * @param to: where the customer is going to
+ * */
+void add_customer(person_t **status, int from, int to);
+
+/** Make the elevators advance
+ * */
+void time_step();
+
+/** Displays current status
+ * */
+void print_system_status();
+
+#endif
