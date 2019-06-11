332794213858ef7dadae99afb902a2672ba48ff6
Marzia Favaro <maarzia.f@gmail.com>
2019-06-11 23:16:28 +0200

    Checked controller.h

--- controller.h
+++ controller.h
@@ -5,18 +5,36 @@
 #include "queue.h"
 #include "parameters.h"
 
+/* Here are defined the functions responsible for the management
+ * of the system
+ * */
+
+
+/** Struct status
+ * Contains the context of the system, that is:
+ * - The queues at each floor
+ * - A boolean vector to keep track of floors that need to be
+ *   served by an elevator
+ * - An array containing the elevators
+ * */
 typedef struct status {
 	// Each index represents a floor, and is a pointer to a queue
 	list_identifier_t floors_queues[FLOORS];
+
 	// Floors which need to be served
-	int to_serve_floors[FLOORS];
-	// NUM_ELEVATORS elevators
+	char to_serve_floors[FLOORS];
+
+	// Elevators
 	elevator_t elevators[NUM_ELEVATORS];
+
 } status_t;
 
-/** @return : status
+
+/** Returns a new status initialized to default
+ * @return : status
  * */
 status_t init_status();
+
 
 /** Inserts a customer in the system
  * @param status: the system the customer has to be inserted into
@@ -25,9 +43,11 @@ status_t init_status();
  * */
 void add_customer(status_t *status, int from, int to);
 
+
 /** Make the elevators advance
  * */
 void time_step(status_t *status);
+
 
 /** Displays current status
  * */
